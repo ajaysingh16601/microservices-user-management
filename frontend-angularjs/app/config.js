72c8c3604c607d541e56795d6a413cfef6c2c6f8
ajaysingh16601 <ajaysingh16601@gmail.com>
2026-05-10 02:44:00 +0530

    updated production api urls

--- frontend-angularjs/app/config.js
+++ frontend-angularjs/app/config.js
@@ -4,10 +4,7 @@
   angular
     .module('userManagementApp')
     .constant('AppConfig', {
-      AUTH_URL: 'http://localhost:3001',
-      USER_URL: 'http://localhost:3002'
-      // Change these to your Render URLs for production:
-      // AUTH_URL: 'https://auth-service.onrender.com',
-      // USER_URL: 'https://user-service.onrender.com'
+      AUTH_URL: 'https://auth-service-4cew.onrender.com',
+      USER_URL: 'https://user-service-7m1w.onrender.com'
     });
 })();
